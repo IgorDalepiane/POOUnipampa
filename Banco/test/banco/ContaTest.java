fe7f1e42754f79764359ca9c97cd5935da44d2cf
Igor Dalepiane <igord@DESKTOP-KFHLKHO>
2019-08-15 06:56:24 -0300

    JUnit Implement

---
+++ Banco/test/banco/ContaTest.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package banco;
+import static junit.framework.Assert.*;
+import org.junit.Test;
+import org.junit.*;
+/**
+ *
+ * @author igord
+ */
+public class ContaTest {
+    
+    private Conta c;
+    
+    public ContaTest() {
+        c = new Conta("Igor");
+        c.deposita(500);
+    }
+    //@Test
+    public void testGetTitular() {
+        assertEquals("Igor", c.getTitular());
+    }
+
+    //@Test
+    public void testGetNumero() {
+        assertEquals(1, c.getNumero());
+    }
+
+   // @Test
+    public void testGetSaldo() {
+    }
+
+    //@Test
+    public void testGetLimite() {
+    }
+
+   // @Test
+    public void testGetSenha() {
+    }
+
+    //@Test
+    public void testIsSenhaCorreta() {
+        assertEquals(true,c.isSenhaCorreta("1234"));
+    }
+
+   // @Test
+    public void testDeposita() {
+        
+    }
+
+    @Test
+    public void testSaca() {
+        assertTrue(c.saca(100));
+    }
+
+    //@Test
+    public void testToString() {
+    }
+    
+}
