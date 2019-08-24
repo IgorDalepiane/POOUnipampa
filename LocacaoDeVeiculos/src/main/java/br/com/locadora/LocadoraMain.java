e1cc344b5c64fed0e36337a4c802fb73af7486a8
Lucas <lucasafell+github@gmail.com>
2019-08-24 03:05:37 -0300

    adicionado o trigger do JFrame da classe ClienteGUI

--- LocacaoDeVeiculos/src/main/java/br/com/locadora/LocadoraMain.java
+++ LocacaoDeVeiculos/src/main/java/br/com/locadora/LocadoraMain.java
@@ -5,10 +5,19 @@
  */
 package br.com.locadora;
 
+import br.com.locadora.clientes.ClienteGUI;
+
+import javax.swing.*;
+
 /**
- *
  * @author 1801560700
  */
 public class LocadoraMain {
-    
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new ClienteGUI().setVisible(true);
+            }
+        });
+    }
 }
