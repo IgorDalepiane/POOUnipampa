1c73778ee35077cfde020aa1916e058a67422b9c
Lucas <lucasafell+github@gmail.com>
2019-08-25 15:22:38 -0300

    código separado em módulos

--- LocacaoDeVeiculos/src/main/java/br/com/locadora/clientes/ClienteGUI.java
+++ LocacaoDeVeiculos/src/main/java/br/com/locadora/clientes/ClienteGUI.java
@@ -1,65 +1,31 @@
 package br.com.locadora.clientes;
 
+import br.com.locadora.clientes.GUI.Toolbar;
+
 import javax.swing.*;
 import java.awt.*;
 
+//controller
 public class ClienteGUI extends JFrame {
-    private JTextField nomeTextField, cpfTextField, cnhTextField, telefoneTextField, enderecoTextField;
-    private JLabel label;
-    private JButton cadastrar;
 
+    private Toolbar t;
 
-    public ClienteGUI()  {
-        createView();
-        setTitle("CRUD Cliente");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    private Cliente c;
+
+    public ClienteGUI() {
+        super("Cliente CRUD");
+
+        setLayout(new BorderLayout());
+        setPreferredSize(new Dimension(300, 80));
         pack();
         setLocationRelativeTo(null);
-        setResizable(false);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        generate();
     }
 
-    private void createView() {
-        //cria um painel no JFrame
-        JPanel p = new JPanel();
-        getContentPane().add(p);
-
-
-        label = new JLabel("Nome:");
-        p.add(label);
-
-        nomeTextField = new JTextField();
-        nomeTextField.setPreferredSize(new Dimension(100, 30));
-        p.add(nomeTextField);
-
-        label = new JLabel("CPF:");
-        p.add(label);
-
-        cpfTextField = new JTextField();
-        cpfTextField.setPreferredSize(new Dimension(100, 30));
-        p.add(cpfTextField);
-
-        label = new JLabel("CNH:");
-        p.add(label);
-
-        cnhTextField = new JTextField();
-        cnhTextField.setPreferredSize(new Dimension(100, 30));
-        p.add(cnhTextField);
-
-        label = new JLabel("Telefone:");
-        p.add(label);
-
-        telefoneTextField = new JTextField();
-        telefoneTextField.setPreferredSize(new Dimension(100, 30));
-        p.add(telefoneTextField);
-
-        label = new JLabel("Endereço:");
-        p.add(label);
-
-        enderecoTextField = new JTextField();
-        enderecoTextField.setPreferredSize(new Dimension(100, 30));
-        p.add(enderecoTextField);
-
-        cadastrar = new JButton("Cadastrar");
-        p.add(cadastrar);
+    private void generate() {
+        t = new Toolbar();
+        add(t, BorderLayout.NORTH);
     }
 }
