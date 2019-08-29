ff9fbd69b440602a50b61998bd6227702cb8a9df
Lucas <lucasafell+github@gmail.com>
2019-08-29 01:13:40 -0300

    typo

--- LocacaoDeVeiculos/src/main/java/br/com/locadora/clientes/IClientes.java
+++ LocacaoDeVeiculos/src/main/java/br/com/locadora/clientes/IClientes.java
@@ -8,14 +8,14 @@ public interface IClientes {
      * Adiciona um cliente na relação de clientes.
      * @param c Cliente a ser inserido.
      */
-    public void add(Cliente c);
+    void add(Cliente c);
     
     /**
      * Captura o cliente com o CPF informado por parâmetro.
      * @param CPF CPF do cliente a ser capturado.
      * @return Cliente com o CPF informado ou null caso o CPF não for encontrado.
      */
-    public Cliente get(long CPF);
+    Cliente get(long CPF);
     
     /**
      * Captura uma String com as informaçoes do cliente com CPF informado por parâmetro.
@@ -23,30 +23,30 @@ public interface IClientes {
      * @return String com as informaçoes do cliente com o CPF informado por parâmetro 
      * ou null caso o CPF não for encontrado.
      */
-    public String getInfo(long CPF);
+    String getInfo(long CPF);
     
     /**
      * Captura uma String com as informaçoes de todos os clientes.
      * @return String com as informaçoes de todos os clientes
      * ou null caso não exista nenhum cliente.
      */
-    public String getInfo();
+    String getInfo();
     
     /**
      * Captura uma String com o CPF e o nome de todos os clientes.
      * @return String com o CPF e o nome de todos os clientes
      * ou null caso não exista nenhum cliente.
      */
-    public String getResumoInfo();
+    String getResumoInfo();
     
     /**
      * Modifica as informações do cliente com o CPF informado por parâmetro.
-     * @param CPF CPD do cliente a ser modificado.
+     * @param CPF CPF do cliente a ser modificado.
      * @param c Cliente com as modificações.
      * @return True se o cliente com o CPF informado por parâmetro for modificado ou
      * false caso não exista nenhum cliente com o CPF informado.
      */
-    public boolean set(long CPF, Cliente c);
+    boolean set(long CPF, Cliente c);
     
     /**
      * Remove o cliente com o CPF igual ao informado por parâmetro.
@@ -54,7 +54,7 @@ public interface IClientes {
      * @return True se o cliente com o CPF informado por parâmetro for removido ou
      * false caso não exista nenhum cliente com o CPF informado. 
      */
-    public boolean remove(long CPF);
+    boolean remove(long CPF);
     
     /**
      * Verifica se existe um cliente com o CPF informado por parâmetro.
@@ -62,5 +62,5 @@ public interface IClientes {
      * @return True se um cliente com o CPF informado for encontrado ou 
      * false caso não exista nenhum cliente com o CPF informado.
      */
-    public boolean existe(long CPF);
+    boolean existe(long CPF);
 }
